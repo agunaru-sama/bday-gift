cc4be0ded9bf72a123a8540847aa81dd013e3bf1
agunaru-sama <98889306+agunaru-sama@users.noreply.github.com>
2026-01-13 13:36:03 +0700

    Delete drag paper directory

--- drag paper/next.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener("DOMContentLoaded", () => {
-  const nextBtn = document.getElementById("nextBtn");
-
-  nextBtn.addEventListener("click", () => {
-    window.location.href = "../next/index.html"; 
-    // ganti sesuai folder HTML berikutnya
-  });
-});
